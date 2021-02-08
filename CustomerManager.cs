239c49a65faafa7235dd74620db71ad1cee39f72
deimari <65736373+deimari@users.noreply.github.com>
2021-02-08 17:33:24 +0300

    Add files via upload

---
+++ CustomerManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        List<Customer> customerList;
+        public CustomerManager()
+        {
+            customerList = new List<Customer>();
+        }
+        public void Add(Customer customer)
+        {
+            customerList.Add(customer);
+            Console.WriteLine(customer.Name + " " + customer.LastName + " added");
+        }
+        public void Remove(Customer customer)
+        {
+            customerList.Remove(customer);
+            Console.WriteLine(customer.Name + " " + customer.LastName + " removed");
+        }
+        public void ListCustomers()
+        {
+            foreach (Customer customer in customerList)
+            {
+                Console.WriteLine("==================================");
+                Console.WriteLine("Customer id: "  + customer.Id);
+                Console.WriteLine("Customer name: " + customer.Name);
+                Console.WriteLine("Customer lastname:  " + customer.LastName);
+                Console.WriteLine("==================================");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer { Id = 1, Name = "Ahmet", LastName = "adasdasd" };
+            Customer customer2 = new Customer { Id = 2, Name = "Mehmet", LastName = "sadasda" };
+            Customer customer3 = new Customer { Id = 3, Name = "Veli", LastName = "dasdasd" };
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+            customerManager.Add(customer3);
+            customerManager.ListCustomers();
+            customerManager.Remove(customer2);
+            customerManager.ListCustomers();
+        }
+    }
+}
